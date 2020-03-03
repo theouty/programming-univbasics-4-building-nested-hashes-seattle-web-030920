32723ca9ccfce7f42f853de81e0087c02ee697e8
Aman Michael <aman.david.michael@gmail.com>
2020-03-03 08:09:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,8 +30,8 @@ def monopoly_with_fourth_tier
   monopoly = {
 	  :railroads => {
 	    :pieces =>4,
-	    :rent_in_dollars => {:one_piece_owned =>25,
-:two_pieces_owned =>50,
+	    :rent_in_dollars => {:one_piece_owned => 25,
+        :two_pieces_owned =>50,
         :three_pieces_owned =>100,
         :four_pieces_owned =>200
 	    },
